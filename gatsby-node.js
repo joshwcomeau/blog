ced5ac38cd245c4baf64835191e8b878291441cf
Josh Comeau <joshwcomeau@gmail.com>
2018-05-01 08:41:01 -0400

    Revert "Update webpack config"
    Turns out I don't think I need MDX
    
    This reverts commit fa51743f3fdac37f559ed36800c4a36b6f7caee5.

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,26 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
-
-const generateBabelConfig = require('gatsby/dist/utils/babel-config');
-
-exports.modifyWebpackConfig = ({ config, stage }) => {
-  switch (stage) {
-    case 'develop':
-    case 'build-javascript': {
-      config._loaders.md = {
-        config: {
-          test: /\.md$/,
-          loaders: ['babel-loader', '@mdx-js/loader'],
-        },
-      };
-
-      console.log(config._loaders);
-      return config;
-    }
-
-    default:
-      return config;
-  }
-};
+ // You can delete this file if you're not using it
