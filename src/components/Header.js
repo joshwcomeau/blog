eab07ff93fcdca63f40d6c477ab729973bf6f5a7
Josh Comeau <joshwcomeau@gmail.com>
2018-05-04 08:41:43 -0400

    Add Home icon, finish handling scroll-to-reveal logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,32 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
-import Icon from 'react-icons-kit';
+import { iosHome } from 'react-icons-kit/ionicons/iosHome';
+import { iosHomeOutline } from 'react-icons-kit/ionicons/iosHomeOutline';
 
 import { COLORS, Z_INDICES } from '../constants';
 import { humanizeDate } from '../helpers/date.helpers';
 import { clamp } from '../utils';
 
+import ClickableIcon from './ClickableIcon';
+
 const HEIGHT = 50;
+const BUFFER = 75;
+
+// TODO: Right now, 3 things are dependent on a scroll threshold:
+//   - This Header
+//   - LargeScreenSidebar
+//   - BaseHero
+//
+// It would be great if all 3 drew on the same value (even if the numbers
+// aren't identical, they should all be derived from the same root value).
+const SCROLL_THRESHOLD = 66;
+
+const getIsWithinVisibleRange = scrollAmount =>
+  scrollAmount / window.innerHeight * 100 > SCROLL_THRESHOLD;
 
 class Header extends PureComponent {
   state = {
+    isWithinVisibleRange: false,
     translateAmount: 0,
   };
 
@@ -26,12 +43,17 @@ class Header extends PureComponent {
   }
 
   handleScroll = () => {
-    const newScrollPosition = window.scrollY;
+    const currentScrollPosition = window.scrollY;
 
-    const newScrollDirection =
-      newScrollPosition > this.scrollPosition ? 'down' : 'up';
+    const isWithinVisibleRange = getIsWithinVisibleRange(currentScrollPosition);
+    if (isWithinVisibleRange !== this.state.isWithinVisibleRange) {
+      this.setState({ isWithinVisibleRange });
+    }
 
-    if (this.scrollDirection === 'down' && newScrollDirection === 'up') {
+    const currentScrollDirection =
+      currentScrollPosition > this.scrollPosition ? 'down' : 'up';
+
+    if (this.scrollDirection === 'down' && currentScrollDirection === 'up') {
       // We don't want to allow the locked position to be changed if we're
       // in the middle of showing/hiding the header; this causes an odd glitchy
       // effect.
@@ -40,36 +62,57 @@ class Header extends PureComponent {
       if (!isMidReveal) {
         this.lockedScrollPosition = this.scrollPosition;
       }
-    } else if (this.scrollDirection === 'up' && newScrollDirection === 'down') {
-      const isMidReveal = this.state.translateAmount !== HEIGHT;
+    } else if (
+      this.scrollDirection === 'up' &&
+      currentScrollDirection === 'down'
+    ) {
+      const isMidReveal = this.state.translateAmount !== HEIGHT + BUFFER;
 
       if (!isMidReveal) {
-        this.lockedScrollPosition = this.scrollPosition + HEIGHT;
+        this.lockedScrollPosition = this.scrollPosition + HEIGHT + BUFFER;
       }
     }
 
     const translateAmount = clamp(
-      this.lockedScrollPosition - newScrollPosition,
+      this.lockedScrollPosition - currentScrollPosition,
       0,
-      HEIGHT
+      HEIGHT + BUFFER
     );
 
-    this.setState({ translateAmount });
+    this.scrollDirection = currentScrollDirection;
+    this.scrollPosition = currentScrollPosition;
 
-    this.scrollDirection = newScrollDirection;
-    this.scrollPosition = newScrollPosition;
+    if (translateAmount !== this.state.translateAmount) {
+      this.setState({ translateAmount });
+    }
   };
 
   render() {
     const { title, publishedOn, heroStyle } = this.props;
-    const { translateAmount } = this.state;
+    const { translateAmount, isWithinVisibleRange } = this.state;
 
     // TODO: color should depend on heroStyle.
     // Also, `heroStyle` should be renamed.
     const color = COLORS.pink[300];
 
     return (
-      <Wrapper style={{ transform: `translateY(${translateAmount}px)` }}>
+      <Wrapper
+        style={{
+          transform: `translateY(${translateAmount}px)`,
+          opacity: isWithinVisibleRange ? 1 : 0,
+        }}
+      >
+        <IconWrapper>
+          <ClickableIcon
+            href="/"
+            icon={iosHomeOutline}
+            iconHover={iosHome}
+            size={32}
+            color={COLORS.gray[300]}
+            colorHover={COLORS.white}
+          />
+        </IconWrapper>
+
         <span>
           <Title color={color}>{title}</Title>
           <Date>{humanizeDate(publishedOn)}</Date>
@@ -85,11 +128,14 @@ const Wrapper = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  top: ${-HEIGHT}px;
-  left: 0;
-  right: 0;
+  top: ${-HEIGHT - BUFFER + 10}px;
+  left: 10px;
+  right: 10px;
   height: ${HEIGHT}px;
+  color: ${COLORS.gray[300]};
   background: ${COLORS.gray[800]};
+  transition: opacity 250ms;
+  will-change: transform;
 `;
 
 const Title = styled.span`
@@ -101,8 +147,17 @@ const Title = styled.span`
 const Date = styled.span`
   display: inline-block;
   padding-left: 1rem;
-  color: ${COLORS.gray[300]};
   font-size: 1rem;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  left: 12px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
 `;
 
 export default Header;
