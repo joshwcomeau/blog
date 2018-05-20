86227ab9673ce0fa4360e7ff531e206cd12d2672
Josh Comeau <joshua@khanacademy.org>
2018-05-20 13:09:22 +0200

    Remove the Whimsy post

--- src/all-posts.js
+++ src/all-posts.js
@@ -3,10 +3,7 @@
 // Maybe if/when I move to MDX, this problem will be solved automatically?
 
 import { FRONT_MATTER as bezier } from './pages/posts/dynamic-bezier-curves';
-import { FRONT_MATTER as whimsy } from './pages/posts/the-case-for-whimsy';
 
-const posts = [bezier, whimsy].sort(
-  (a, b) => (a.publishedOn > b.publishedOn ? -1 : 1)
-);
+const posts = [bezier].sort((a, b) => (a.publishedOn > b.publishedOn ? -1 : 1));
 
 export default posts;
