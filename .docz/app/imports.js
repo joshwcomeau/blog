69b806c194184998e3d5a4eab08e5c167c880daf
Josh Comeau <joshwcomeau@gmail.com>
2018-10-07 14:40:10 -0400

    Ignore docz cache from Git

--- .docz/app/imports.js
+++
@@ -1,4 +0,0 @@
-export const imports = {
-  'src/components/MultipleChoiceControl/MultipleChoiceControl.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-multiple-choice-control-multiple-choice-control" */ 'src/components/MultipleChoiceControl/MultipleChoiceControl.mdx'),
-}
