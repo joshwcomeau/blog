0c906da86a0f32b73b3acacf78f1fe0033a2db0c
Joshua Comeau <joshwcomeau@gmail.com>
2019-05-12 18:55:15 -0400

    Fix blog post rendering

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
-import { MDXProvider } from '@mdx-js/tag';
+import { MDXProvider } from '@mdx-js/react';
 
 import { siteMetadata } from '../../../gatsby-config';
 
