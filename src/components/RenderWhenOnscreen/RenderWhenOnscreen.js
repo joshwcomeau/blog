d726e2b2bfbb05ccbfc80aa071b101c5c8ce594d
Joshua Comeau <joshwcomeau@gmail.com>
2019-05-13 07:03:42 -0400

    Add helper component to disable offscreen animations

---
+++ src/components/RenderWhenOnscreen/RenderWhenOnscreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { throttle } from '../../utils';
+
+const BUFFER = 100;
+
+const useIsOnscreen = elementRef => {
+  const [isOnscreen, setIsOnscreen] = React.useState(true);
+
+  React.useEffect(
+    () => {
+      const handleScroll = throttle(() => {
+        if (!elementRef.current) {
+          return;
+        }
+
+        const bb = elementRef.current.getBoundingClientRect();
+
+        const isWithinViewport =
+          bb.top <= window.innerHeight + BUFFER && bb.bottom > -BUFFER;
+
+        if (isWithinViewport && !isOnscreen) {
+          setIsOnscreen(true);
+        } else if (!isWithinViewport && isOnscreen) {
+          setIsOnscreen(false);
+        }
+      }, 200);
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    },
+    [elementRef]
+  );
+
+  return isOnscreen;
+};
+
+const RenderWhenOnscreen = ({ height, children }) => {
+  const ref = React.createRef(null);
+
+  const isOnscreen = useIsOnscreen(ref);
+
+  return (
+    <div ref={ref} style={{ height }}>
+      {isOnscreen && children}
+    </div>
+  );
+};
+
+export default RenderWhenOnscreen;
